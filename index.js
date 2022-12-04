c2ced335919590c22b07378d1119c265371b46d3
DhanushM12 <dhanushmukhi@gmail.com>
2022-12-04 16:54:26 +0530

    added ui and other config

--- index.js
+++ index.js
@@ -1,12 +1,44 @@
+require('dotenv').config()
+
 const express = require('express');
 const port = 8000;
 const app = express();
 const passport = require('passport')
-
+const bcrypt = require('bcrypt')
+const session = require('express-session')
 const users = [];
 
+const initialize = require('./config/passportLocal');
+initialize(passport, 
+    email => users.find(user => user.email == email),
+    id => users.find(user => user.id == id)
+    );
+
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({extended: false}))
+
+app.use(session({
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: false
+}))
+
+passport.use(passport.initialize());
+passport.use(passport.session());
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', {name: "april22"})
+})
 
 
+app.get('/login', (req, res) => {
+    res.render('login.ejs')
+})
+
+app.get('/register', (req, res) => {
+    res.render('register.ejs')
+})
 
 app.listen(port, function(err){
     if(err){
